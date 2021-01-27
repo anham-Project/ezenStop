e79917401b8547f210ffc58984c69ddaf5c77166
luyer1325 <luyer999999999@gmail.com>
2021-01-27 15:36:27 +0900

    no message

---
+++ src/main/java/com/ezen709/ezenStop/model/BlockDTO.java
@@ -0,0 +1,25 @@
+package com.ezen709.ezenStop.model;
+
+public class BlockDTO {
+	private String id;
+	private String blockId;
+	private String memo;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getBlockId() {
+		return blockId;
+	}
+	public void setBlockId(String blockId) {
+		this.blockId = blockId;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+}
